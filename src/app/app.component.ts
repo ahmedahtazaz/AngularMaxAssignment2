b5f5e0f3670d3753fc0a9aa19b7df0218d9af5b7
Ahtazaz Ahmed Khan <49054500+ahmedahtazaz@users.noreply.github.com>
2020-09-17 11:07:36 +0500

    Max assignmet2 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'udemy-max-assignment2';
+  name = 'ahtazaz';
+
+  getIsButtonDisabled = () => {
+    return this.name == '';
+  }
+
+  onButtonClicked = () => {
+    this.name = '';
+  }
 }
